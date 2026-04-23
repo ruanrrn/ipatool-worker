5bc8d55ada37585c6701e383493bf500b85b0ed2
RayRuan <rrnruan@gmail.com>
2026-04-23 23:52:42 +0800

    chore: add server/dist to .gitignore

--- server/dist/assets/api-utils-CrS9-M9f.js
+++
@@ -1,1 +0,0 @@
-const c=async(e,s={})=>{const n={...s,credentials:"include"},a=await fetch(e,n);let t;try{t=await a.json()}catch{t={ok:!1,error:"Invalid response format"}}return{response:a,data:t}};export{c as a};
